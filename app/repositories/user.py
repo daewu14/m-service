790eb3689e6c0eda7955953bb7c110478f9fd60f
daewu <daewu.bintara1996@gmail.com>
2025-02-17 00:25:23 +0700

    improve : refactor on detail user case, will improve again soon

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -4,5 +4,5 @@ from pkg.db import alchemy_tx, sql_text
 class UserRepository:
     @classmethod
     def find_by_id(cls, user_id):
-        result = alchemy_tx.fetchall(query=sql_text(f"SELECT * FROM users WHERE id=:id"), arguments={'id': user_id})
+        result = alchemy_tx.fetchall(query=sql_text(f"SELECT * FROM users WHERE id=:id limit 1"), arguments={'id': user_id})
         return result
--- app/ucase/user/detail.py
+++ app/ucase/user/detail.py
@@ -1,6 +1,7 @@
 import app.ucase.user as user_case
 from pkg.logger.log import logger
 from pkg.http import response, http_status
+import json
 
 response_docs = {
     200: {
@@ -36,17 +37,12 @@ response_docs = {
 
 async def user_by_id(user_id: int):
     user = await user_case.user_repository.find_by_id(user_id)
-    logger.info("user-detail", extra={"user-detail": user})
-    if user is None:
+    logger.info("user-detail", extra={"user-detail": user[0]})
+    if user is None or len(user) == 0:
         return response(status=http_status.NOT_FOUND, message="User Not Found")
 
+    # user = json.loads(f"{user[0]}")
     return response(
         status=http_status.OK,
         message="User Found",
-        data={
-            "id": user[0],
-            "uuid": user[1],
-            "name": user[2],
-            "email": user[3],
-        }
     )
